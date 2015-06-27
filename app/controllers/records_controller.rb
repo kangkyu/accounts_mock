00aecbf0a3e2716cb174799b3e0f07f2d2fd9e18
kangkyu <kangkyu1111@gmail.com>
2015-06-26 19:26:50 -0700

    add a form creating records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -3,4 +3,19 @@ class RecordsController < ApplicationController
   def index
     @records = Record.all
   end
+
+  def create
+    @record = Record.new(record_params)
+    if @record.save
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entitiy
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:title, :date, :amount)
+  end
 end
